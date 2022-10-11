e52690b5933fc5340e298f4a7f48acc23053eb77
casm3 <casm3@cin.ufpe.br>
2022-10-11 19:56:45 -0300

    Exercise Hashmap - States x Population.

--- src/br/com/collections/map/Exercise1.java
+++ src/br/com/collections/map/Exercise1.java
@@ -23,5 +23,81 @@ package br.com.collections.map;
 // - Clear the dict;
 // - Check if the dict is empty.
 
+import java.util.*;
+
 public class Exercise1 {
+    public static void main(String[] args) {
+        Map<String, Integer> states = new HashMap<>(){{
+            put("PE", 9_616_621);
+            put("AL", 3_351_543);
+            put("CE", 9_187_103);
+            put("RN", 3_534_265);
+        }};
+
+        // Change the population of the RN State for 3.534.165;
+        states.put("RN", 3_534_165);
+
+        // Check if the PB state is in the dictionary, and add PB – 4.039.277 if it isn't;
+        if (!states.containsKey("PB")){
+            states.put("PB", 4_039_277);
+        }
+
+        // — Present the PE population;
+        System.out.println("PE - Population: " + states.get("PE"));
+
+
+        // — Show all states, and their population in the insertion order;
+        Map<String, Integer> linkedStates = new LinkedHashMap<>(){{
+            put("PE", 9_616_621);
+            put("AL", 3_351_543);
+            put("CE", 9_187_103);
+            put("RN", 3_534_265);
+        }};
+        System.out.println(linkedStates);
+
+        // - Show all states, and their population in the alphabetical order;
+        Map<String, Integer> treeMapStates = new TreeMap<>(linkedStates);
+        System.out.println(treeMapStates);
+
+        // — Show the state with min population and the amount;
+        Collection<Integer> population = states.values();
+        Integer minPopulation = Collections.min(population);
+        Integer maxPopulation = Collections.max(population);
+        String stateWithMinPopulation = "";
+        String stateWithMaxPopulation = "";
+
+        Set<Map.Entry<String, Integer>> entries = states.entrySet();
+        for (Map.Entry<String, Integer> state : entries) {
+            if (state.getValue().equals(minPopulation)) {
+                stateWithMinPopulation = state.getKey();
+                System.out.println("State: "
+                        + stateWithMinPopulation
+                        + " - Population: "
+                        + minPopulation
+                );
+            }
+            if (state.getValue().equals(maxPopulation)) {
+                    stateWithMaxPopulation = state.getKey();
+                    System.out.println("State: "
+                        + stateWithMaxPopulation
+                        + " - Population: "
+                        + maxPopulation
+                );
+                }
+        }
+        // — Remove all states with population smaller than 4_000_000;
+        states.values().removeIf(aDouble -> aDouble.equals(4_000_000));
+
+        // — Show the sum of all populations;
+        int sum = 0;
+        for (int value : population){
+            sum += value;
+        }
+        System.out.println(sum);
+
+        // — Clear the dict;—
+        // Check if the dict is empty.
+        states.clear();
+        System.out.println(states.isEmpty());
+    }
 }
